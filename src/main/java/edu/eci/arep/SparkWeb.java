013b1b77c412639371d1b798e4598da431bfdfc7
diego <diegofernandoruizrojas@gmail.com>
2022-09-28 22:46:42 -0500

    conexion a mongo

--- src/main/java/edu/eci/arep/SparkWeb.java
+++ src/main/java/edu/eci/arep/SparkWeb.java
@@ -1,14 +1,40 @@
 package edu.eci.arep;
 
 
-import static spark.Spark.port;
-import static spark.Spark.get;
+import edu.eci.arep.repository.MongoDbRepository;
+
+import java.util.ArrayList;
+
+import static spark.Spark.*;
 
 public class SparkWeb {
 
+
     public static void main(String... args){
+        MongoDbRepository mongoDbRepository = new MongoDbRepository();
         port(getPort());
-        get("hello", (req,res) -> "Hello Docker!");
+        staticFiles.location("/public");
+        get("message", (req,res) -> {
+            mongoDbRepository.createConnection();
+
+            ArrayList<String> messages = mongoDbRepository.getAllItems();
+
+            mongoDbRepository.closeConnection();
+            return messages;
+        });
+        post("message", (req,res) -> {
+            mongoDbRepository.createConnection();
+
+            if(req.body()!=null){
+                mongoDbRepository.addItem(req.body());
+            }
+
+            ArrayList<String> messages = mongoDbRepository.getAllItems();
+
+            mongoDbRepository.closeConnection();
+
+            return messages;
+        });
     }
 
     private static int getPort() {
